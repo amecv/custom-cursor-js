82f78d9a4c23ae2e938b1a20bafc2eaa0b86fa39
Amec Velasquez <amecvelasquez@gmail.com>
2022-08-27 19:16:51 -0600

    Initial Commit

---
+++ customCursor.js
@@ -0,0 +1,134 @@
+// List of elements to interact width, in CSS selector format
+const interactWith = 'button, a';
+
+// Initialize custom cursor
+startCustomCursor(interactWith);
+
+function startCustomCursor(interactionSelector = '', sticky=true) {
+    //don't do anything on touch screens
+    if(!(( 'ontouchstart' in window ) || ( navigator.maxTouchPoints > 0 ) || ( navigator.msMaxTouchPoints > 0 ))) {
+        //Select elements
+        const cursorContainer = document.querySelector('.has-custom-cursor');
+        const cursorElement = document.getElementById('custom-cursor');
+        const cursorElementRect = cursorElement.getBoundingClientRect();
+        const buttonsQuery = interactionSelector.split(',').map((i) => i = '.has-custom-cursor ' + i.trim()).join(', ');
+        const buttons = document.querySelectorAll(buttonsQuery);
+
+        //Prep for animation: adding 'will-change: transform' improves animation performance
+        Array.from(buttons).some((btn) => {
+            btn.style.willChange = 'transform';
+        });
+
+        //Get CSS options
+        const cursorSize = parseInt(getComputedStyle(cursorContainer).getPropertyValue('--cursor-size'));
+        const cursorInteractionPadding = parseInt(getComputedStyle(cursorContainer).getPropertyValue('--cursor-interaction-padding'));
+        const cursorBorderWidth = parseInt(getComputedStyle(cursorContainer).getPropertyValue('--cursor-border-width'));
+
+        //Cursor Options
+        const cursorOptions = {
+            cursorElement: cursorElement,
+            cursorPositionOffset: cursorElementRect.height / 2,
+            cursorSize: cursorSize,
+            cursorInteractionPadding: cursorInteractionPadding,
+            cursorInteractionBuffer: (cursorSize / 2) + (cursorBorderWidth * 2) + cursorInteractionPadding,
+            cursorBorderWidth: cursorBorderWidth,
+            cursorButtonOffset: cursorInteractionPadding + (cursorBorderWidth * 2)
+        }
+
+        // Update cursor position and check for interactions 
+        window.onmousemove = (e) => {
+            window.requestAnimationFrame(() => {
+                updateCursor(cursorOptions, e);
+                checkForCursorInteraction(e, cursorOptions, buttons, sticky);
+            });
+        };
+
+        //For animating mouse clicks
+        window.onmousedown = (e) => {
+            window.mouseClicked = true;
+            updateCursor(cursorOptions, e);
+        }
+
+        window.onmouseup = (e) => {
+            window.mouseClicked = false;
+            updateCursor(cursorOptions, e);
+        }
+    }
+}
+
+function updateCursor(cursorOptions, e) {
+    //Pause if the cursor is interacing with something
+    if(!window.cursorInteraction) {
+        //how fast is the the cursor moving
+        const speedX = Math.abs(e.movementX);
+        const speedY = Math.abs(e.movementY);
+        const speed = Math.max(speedX, speedY);
+        
+        //squish the cursor when it moves fast
+        const scaleX = speedY > 2 ? 0.85 : 1;
+        const scaleY = speedX > 2 ? 0.85 : 1;
+        
+        //click animation
+        const scale = window.mouseClicked ? 0.8 : `${scaleX}, ${scaleY}`;
+        
+        //calculate position
+        const positionX = e.clientX - cursorOptions.cursorPositionOffset;
+        const positionY = e.clientY - cursorOptions.cursorPositionOffset;
+        
+        //set cursor transform
+        cursorOptions.cursorElement.style.transform = `translateX(${positionX}px) translateY(${positionY}px) scale(${scale})`;
+    }
+}
+
+function checkForCursorInteraction(e, cursorOptions, buttonList, sticky) {
+    //"sticky" option makes the interaction start before the mouse touches the element
+    const interactionBuffer = sticky ? cursorOptions.cursorInteractionBuffer : 0;
+    
+    //Check each button and calculate positions and stuff
+    Array.from(buttonList).some((btn) => {
+        const buttonPosition = btn.getBoundingClientRect();
+        const interactionTop = buttonPosition.top - interactionBuffer;
+        const interactionBottom = buttonPosition.top + buttonPosition.height + interactionBuffer;
+        const interactionLeft = buttonPosition.left - interactionBuffer;
+        const interactionRight = buttonPosition.left + buttonPosition.width + interactionBuffer;
+        const buttonTransform = {
+            x: buttonPosition.left + buttonPosition.width / 2,
+            y: buttonPosition.top + buttonPosition.height / 2,
+        }
+
+        //click animation
+        btn.onmousedown = () => btn.style.transform = btn.style.transform + ` scale(0.95)`;
+        btn.onmouseup = () => btn.style.transform = btn.style.transform.replace(` scale(0.95)`, '');
+        
+        //reset animation when the mouse leaves
+        btn.onmouseleave = () => btn.style.transform = '';
+
+        //if the cursor is interacting with this button, animate it
+        if (
+            e.clientY >= interactionTop &&
+            e.clientY <= interactionBottom &&
+            e.clientX >= interactionLeft &&
+            e.clientX <= interactionRight
+        ) {
+            window.cursorInteraction = true;
+            const movementY = (e.clientY - (buttonPosition.top + buttonPosition.height / 2 + interactionBuffer)) / 8;
+            const movementX = (e.clientX - (buttonPosition.left + buttonPosition.width / 2 + interactionBuffer)) / 8;
+            btn.style.transform = `translate(${movementX}px, ${movementY}px)`;
+            cursorOptions.cursorElement.classList.add('active');
+            cursorOptions.cursorElement.style.transform = `translateX(${buttonTransform.x}px) translateY(${buttonTransform.y}px) translate(-50%, -50%)`;
+            cursorOptions.cursorElement.style.width = buttonPosition.width + (cursorOptions.cursorButtonOffset * 2) + 'px';
+            cursorOptions.cursorElement.style.height = buttonPosition.height + (cursorOptions.cursorButtonOffset * 2) + 'px';
+            cursorOptions.cursorElement.style.borderRadius = getComputedStyle(btn).borderRadius ? parseInt(getComputedStyle(btn).borderRadius.replace('px', '')) + cursorOptions.cursorButtonOffset + 'px' : '3px';
+            return true;
+        } else {
+            //if the cursor is not interacting with this button, reset everything
+            window.cursorInteraction = false;
+            btn.style.transform = '';
+            cursorOptions.cursorElement.classList.remove('active');
+            cursorOptions.cursorElement.style.width = '';
+            cursorOptions.cursorElement.style.height = '';
+            cursorOptions.cursorElement.style.transformOrigin = '';
+            cursorOptions.cursorElement.style.borderRadius = '';
+        }
+    });
+}
---
+++ readme.md
@@ -0,0 +1,52 @@
+# Custom Cursor With UI interactions
+
+Using vanilla js and css
+
+## Usage
+
+Add the CSS and Javascript file in your HTML
+
+```html
+    <link rel="stylesheet" href="customCursor.min.css">
+    <script src="customCursor.min.js"></script>
+```
+
+Add the `has-custom-cursor` class to your container, and add a div with id `custom-cursor`
+
+```html
+    <div class="container has-custom-cursor">
+        <div id="custom-cursor"></div>
+        ...
+    </div>
+```
+
+You can customize the cursor using CSS Custom Properties on `.has-custom-cursor`
+
+```css
+    --cursor-size: 20px;
+    --cursor-border-width: 1px;
+    --cursor-transition: all 0.35s cubic-bezier(0.34, 1.56, 0.64, 1);
+    --cursor-interaction-padding: 3px;
+    --cursor-color: #f7b32b;
+
+```
+
+Initialize the custom cursor in your JS
+
+```js
+    startCustomCursor();
+```
+
+**Optional parameters:**
+
+- list of elements to interact with in CSS selector format
+
+```js
+    startCustomCursor('button, a');
+```
+
+- disable "sticky" behaviour by using `false` as a seccond parameter
+
+```js
+    startCustomCursor('button, a');
+```
